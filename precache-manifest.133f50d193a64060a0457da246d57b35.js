af56cf8f205914b074e4595ddda6a0b49e3742e6
Strynzha Roman <johndoe@example.com>
2020-07-31 22:32:01 +0300

    Updates

---
+++ precache-manifest.133f50d193a64060a0457da246d57b35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27764982d41a0e76b12272e528a57ac2",
+    "url": "/react_warrior/index.html"
+  },
+  {
+    "revision": "4a7c370c7a31408fbf71",
+    "url": "/react_warrior/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f07f83af503cb6c7f0ba",
+    "url": "/react_warrior/static/css/main.4ee167ff.chunk.css"
+  },
+  {
+    "revision": "4a7c370c7a31408fbf71",
+    "url": "/react_warrior/static/js/2.658b10b4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react_warrior/static/js/2.658b10b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f07f83af503cb6c7f0ba",
+    "url": "/react_warrior/static/js/main.3838a6b8.chunk.js"
+  },
+  {
+    "revision": "258106c4b466677248f1",
+    "url": "/react_warrior/static/js/runtime-main.513d126c.js"
+  }
+]);
